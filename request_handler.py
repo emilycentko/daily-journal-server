603bbf447b020ef5d0ea720077a9f020c55091a3
emilycentko <emily.centko@gmail.com>
2021-04-21 15:44:35 -0500

    Function to search entries by LIKE in SQL

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from entries import get_all_entries, get_single_entry, delete_entry
+from entries import get_all_entries, get_single_entry, delete_entry, search_entry
 from moods import get_all_moods
 
 class HandleRequests(BaseHTTPRequestHandler):
@@ -21,41 +21,67 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+
+        if "?" in resource:
+
+            param = resource.split("?")[1]  
+            resource = resource.split("?")[0]  
+            pair = param.split("=")  
+            key = pair[0]  
+            value = pair[1] 
+
+            return ( resource, key, value )
+
+        # No query string parameter
+        else:
+
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  
+            except ValueError:
+                pass 
+
+            return (resource, id)  
+    
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
         self._set_headers(200)
         response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
+        parsed = self.parse_url(self.path)
 
-        if resource == "entries":
-            if id is not None:
-                response = f"{get_single_entry(id)}"
+        if len(parsed) == 2:
+            (resource, id) = parsed
 
-            else:
-                response = f"{get_all_entries()}"
+            if resource == "entries":
+                if id is not None:
+                    response = f"{get_single_entry(id)}"
+
+                else:
+                    response = f"{get_all_entries()}"
         
-        if resource == "moods":
-            response = f"{get_all_moods()}"
+            if resource == "moods":
+                response = f"{get_all_moods()}"
+            
+        
+        
+        elif len(parsed) == 3:
+            (resource, key, value) = parsed
 
-        self.wfile.write(response.encode())
-    
-    def parse_url(self, path):
-        path_params = path.split("/")
-        resource = path_params[1]
-        id = None
+            if resource == "entries":
+                if key == "q":
+                    response = f"{search_entry(value)}"
 
-        try:
-            id = int(path_params[2])
-        except IndexError:
-            pass  
-        except ValueError:
-            pass 
+        self.wfile.write(f"{response}".encode())
 
-        return (resource, id)  
-    
     def do_DELETE(self):
         # Set a 204 response code
         self._set_headers(204)
